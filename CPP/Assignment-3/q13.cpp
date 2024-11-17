db58cdf5347eb21dbef531a9fe67875e3be32205
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-18 02:05:30 +0530

    add q13.cpp: implement a program to display string length and capacity

---
+++ CPP/Assignment-3/q13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string str = "Vinay";
+
+  cout << "The length of string is: " << str.length();
+  cout << "The capacity of the string is: " << str.capacity();
+
+  cout << "\n";
+  return 0;
+}
