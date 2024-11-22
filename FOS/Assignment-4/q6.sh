437324866a97cd90411ad9073646d65060db102a
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-22 22:21:26 +0530

    refactor q6.sh: simplify file compression and remove unnecessary cleanup; add q7.sh: implement script to uncompress files separately

--- FOS/Assignment-4/q6.sh
+++ FOS/Assignment-4/q6.sh
@@ -2,9 +2,5 @@
 # 6. Write a shell script that compresses the two file separetely.
 touch file1.txt
 touch file2.txt
-gzip file1.txt >file1.gz
-gzip file2.txt >file2.gz
-rm file1.txt
-rm file2.txt
-rm file1.gz
-rm file2.gz
+gzip file1.txt
+gzip file2.txt
---
+++ FOS/Assignment-4/q7.sh
@@ -0,0 +1,4 @@
+# !/bin/sh
+# 7. Write a shell script that uncompresses the two file separetely.
+gunzip file1.txt.gz
+gunzip file2.txt.gz
