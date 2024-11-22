439742bba3d1f2ebccd82adde4cc5c8aee527cc5
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-22 14:44:34 +0530

    add q3.sh: implement a shell script to display file contents, system version, command help, and a formatted message

---
+++ FOS/Assignment-1/q3.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# 3. Write a shell script to execute following commands
+# • Display contents of file xyz.txt
+cat xyz.txt
+# • Display version of the UNIX?
+uname -r
+# • How would u get online help of cat command?
+cat --help
+# • How to print “this is a three –lineText message”
+printf " This is a \n three line \n text message"
