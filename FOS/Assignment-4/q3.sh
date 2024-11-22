018f825f68908f3828769bb839988768c1e345e9
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-22 21:16:29 +0530

    add q3.sh: implement a script to display disk space used by files with total count

---
+++ FOS/Assignment-4/q3.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# 3. Write a shell script that display disk space used by files in the following criteria:
+# • write counts for all files , not just directories
+du -a
+# • total count
+du -c
