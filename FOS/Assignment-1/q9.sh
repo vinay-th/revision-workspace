f342cc9dac6e1f2063cd8843f0d2d173979cfd56
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-22 15:50:03 +0530

    add q9.sh: implement a script to display file details and hidden files

---
+++ FOS/Assignment-1/q9.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 9. Write a shell script to perform the following:
+# • Display long listing of the file f1
+ls -l f1
+# • Display number of lines, characters of the file f1
+wc f1
+# • Display all the hidden files in current directory
+ls -ld .?*
